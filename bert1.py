2840b2655b72ea3c1bfe77abf503edfc06cb5b89
sak-id <sakuraki.2001@gmail.com>
2023-03-27 17:05:06 +0900

    two task

---
+++ bert1.py
@@ -0,0 +1,479 @@
+import os
+import re
+from tqdm import tqdm
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from datasets import load_dataset
+
+dataset = load_dataset("sst", "default")
+
+data_train = dataset['train']
+X_train = data_train['sentence']
+y_train = np.array(data_train['label'])
+y_train = np.where(y_train >= 0.5, 1, 0)
+
+data_val = dataset['validation']
+X_val = np.array(data_val['sentence'])
+y_val = np.array(data_val['label'])
+y_val = np.where(y_val >= 0.5, 1, 0)
+
+data_test = dataset['test']
+X_test = data_test['sentence']
+y_test = np.array(data_test['label'])
+y_test = np.where(y_test >= 0.5, 1, 0)
+"""
+print(f"train size: {len(X_train)}")
+print(f"val size: {len(X_val)}")
+print(f"test size: {len(X_test)}")
+"""
+#8544,1101,2210 case each
+
+import torch
+
+device = torch.device("cpu")
+
+import nltk
+# Uncomment to download "stopwords"
+nltk.download("stopwords")
+from nltk.corpus import stopwords
+
+def text_preprocessing(s):
+    """
+    - Lowercase the sentence
+    - Change "'t" to "not"
+    - Isolate and remove punctuations except "?"
+    - Remove other special characters
+    - Remove stop words except "not" and "can"
+    - Remove trailing whitespace
+    """
+    s = s.lower()
+    # Change 't to 'not'
+    s = re.sub(r"\'t", " not", s)
+    # Remove @name
+    #s = re.sub(r'(@.*?)[\s]', ' ', s)
+    # Isolate and remove punctuations except '?'
+    s = re.sub(r'([\'\"\.\(\)\!\?\\\/\,])', r' \1 ', s)
+    s = re.sub(r'[^\w\s\?]', ' ', s)
+    # Remove some special characters
+    s = re.sub(r'([\;\:\|•«\n])', ' ', s)
+    # Remove stopwords except 'not' and 'can'
+    s = " ".join([word for word in s.split()
+                  if word not in stopwords.words('english')
+                  or word in ['not', 'can']])
+    # Remove trailing whitespace
+    s = re.sub(r'\s+', ' ', s).strip()
+    
+    return s
+
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# Preprocess text
+X_train_preprocessed = np.array([text_preprocessing(text) for text in X_train])
+X_val_preprocessed = np.array([text_preprocessing(text) for text in X_val])
+X_test_preprocessed = np.array([text_preprocessing(text) for text in X_test])
+
+# Calculate TF-IDF (data vectorized)
+tf_idf = TfidfVectorizer(ngram_range=(1, 3),
+                         binary=True,
+                         smooth_idf=False)
+X_train_tfidf = tf_idf.fit_transform(X_train_preprocessed)
+X_val_tfidf = tf_idf.transform(X_val_preprocessed)
+X_test_tfidf = tf_idf.transform(X_test_preprocessed)
+
+from sklearn.model_selection import StratifiedKFold, cross_val_score
+
+from sklearn.naive_bayes import MultinomialNB
+
+from sklearn.metrics import accuracy_score, roc_curve, auc
+
+
+def evaluate_roc(probs, y_true, filename):
+    
+    #- Print AUC and accuracy on the test set
+    #- Plot ROC
+    #@params    probs (np.array): an array of predicted probabilities with shape (len(y_true), 2)
+    #@params    y_true (np.array): an array of the true values with shape (len(y_true),)
+    
+    preds = probs[:, 1]
+    fpr, tpr, threshold = roc_curve(y_true, preds)
+
+    roc_auc = auc(fpr, tpr)
+    print(f'AUC: {roc_auc:.4f}')
+       
+    # Get accuracy over the test set
+    y_pred = np.where(preds >= 0.5, 1, 0)
+    accuracy = accuracy_score(y_true, y_pred)
+    print(f'Accuracy: {accuracy*100:.2f}%')
+    
+    # Plot ROC AUC
+    plt.title('Receiver Operating Characteristic')
+    plt.plot(fpr, tpr, 'b', label = 'AUC = %0.2f' % roc_auc)
+    plt.legend(loc = 'lower right')
+    plt.plot([0, 1], [0, 1],'r--')
+    plt.xlim([0, 1])
+    plt.ylim([0, 1])
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.savefig(filename)
+
+"""
+# Compute predicted probabilities
+nb_model = MultinomialNB(alpha=1.8)
+#nb_model.fit(X_train_tfidf, y_train)
+y_train2 = np.where(y_train >= 0.5, 1, 0)
+nb_model.fit(X_train_tfidf, y_train2)
+
+probs = nb_model.predict_proba(X_val_tfidf)
+y_val2 = np.where(y_val>=0.5,1,0)
+# Evaluate the classifier
+evaluate_roc(probs, y_val2, 'alpha.png')
+"""
+
+from transformers import BertTokenizer
+
+# Load the BERT tokenizer
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
+
+# Create a function to tokenize a set of texts
+def preprocessing_for_bert(data):
+    """Perform required preprocessing steps for pretrained BERT.
+    @param    data (np.array): Array of texts to be processed.
+    @return   input_ids (torch.Tensor): Tensor of token ids to be fed to a model.
+    @return   attention_masks (torch.Tensor): Tensor of indices specifying which
+                  tokens should be attended to by the model.
+    """
+    # Create empty lists to store outputs
+    input_ids = []
+    attention_masks = []
+
+    # For every sentence...
+    for sent in data:
+        # `encode_plus` will:
+        #    (1) Tokenize the sentence
+        #    (2) Add the `[CLS]` and `[SEP]` token to the start and end
+        #    (3) Truncate/Pad sentence to max length
+        #    (4) Map tokens to their IDs
+        #    (5) Create attention mask
+        #    (6) Return a dictionary of outputs
+        encoded_sent = tokenizer.encode_plus(
+            text=text_preprocessing(sent),  # Preprocess sentence
+            add_special_tokens=True,        # Add `[CLS]` and `[SEP]`
+            max_length=MAX_LEN,                  # Max length to truncate/pad
+            padding='max_length',         # Pad sentence to max length
+            #return_tensors='pt',           # Return PyTorch tensor
+            return_attention_mask=True      # Return attention mask
+            )
+        
+        # Add the outputs to the lists
+        input_ids.append(encoded_sent.get('input_ids'))
+        attention_masks.append(encoded_sent.get('attention_mask'))
+
+    # Convert lists to tensors
+    input_ids = torch.tensor(input_ids)
+    attention_masks = torch.tensor(attention_masks)
+
+    return input_ids, attention_masks
+
+# Concatenate train data and test data
+all_data = np.concatenate([X_train, X_test])
+
+# Encode our concatenated data
+encoded_data = [tokenizer.encode(sent, add_special_tokens=True) for sent in all_data]
+
+# Find the maximum length
+max_len = max([len(sent) for sent in encoded_data])
+print('Max length: ', max_len)
+
+# Specify `MAX_LEN`
+MAX_LEN = 64
+
+# Print sentence 0 and its encoded token ids
+X= np.concatenate([X_train, X_val, X_test])
+token_ids = list(preprocessing_for_bert([X[0]])[0].squeeze().numpy())
+print('Original: ', X[0])
+print('Token IDs: ', token_ids)
+
+# Run function `preprocessing_for_bert` on the train set and the validation set
+print('Tokenizing data...')
+train_inputs, train_masks = preprocessing_for_bert(X_train)
+val_inputs, val_masks = preprocessing_for_bert(X_val)
+
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+
+# Convert other data types to torch.Tensor
+train_labels = torch.tensor(y_train, dtype=torch.long)
+val_labels = torch.tensor(y_val, dtype=torch.long)
+
+# For fine-tuning BERT, the authors recommend a batch size of 16 or 32.
+batch_size = 32
+
+# Create the DataLoader for our training set
+train_data = TensorDataset(train_inputs, train_masks, train_labels)
+train_sampler = RandomSampler(train_data)
+train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=batch_size)
+
+# Create the DataLoader for our validation set
+val_data = TensorDataset(val_inputs, val_masks, val_labels)
+val_sampler = SequentialSampler(val_data)
+val_dataloader = DataLoader(val_data, sampler=val_sampler, batch_size=batch_size)
+
+import torch
+import torch.nn as nn
+from transformers import BertModel
+
+# Create the BertClassfier class
+class BertClassifier(nn.Module):
+    """Bert Model for Classification Tasks.
+    """
+    def __init__(self, freeze_bert=False):
+        """
+        @param    bert: a BertModel object
+        @param    classifier: a torch.nn.Module classifier
+        @param    freeze_bert (bool): Set `False` to fine-tune the BERT model
+        """
+        super(BertClassifier, self).__init__()
+        # Specify hidden size of BERT, hidden size of our classifier, and number of labels
+        D_in, H, D_out = 768, 50, 2
+
+        # Instantiate BERT model
+        self.bert = BertModel.from_pretrained('bert-base-uncased')
+
+        # Instantiate an one-layer feed-forward classifier
+        self.classifier = nn.Sequential(
+            nn.Linear(D_in, H),
+            nn.ReLU(),
+            #nn.Dropout(0.5),
+            nn.Linear(H, D_out)
+        )
+        # Freeze the BERT model
+        if freeze_bert:
+            for param in self.bert.parameters():
+                param.requires_grad = False
+        
+    def forward(self, input_ids, attention_mask):
+        """
+        Feed input to BERT and the classifier to compute logits.
+        @param    input_ids (torch.Tensor): an input tensor with shape (batch_size,
+                      max_length)
+        @param    attention_mask (torch.Tensor): a tensor that hold attention mask
+                      information with shape (batch_size, max_length)
+        @return   logits (torch.Tensor): an output tensor with shape (batch_size,
+                      num_labels)
+        """
+        # Feed input to BERT
+        outputs = self.bert(input_ids=input_ids,
+                            attention_mask=attention_mask)
+        
+        # Extract the last hidden state of the token `[CLS]` for classification task
+        last_hidden_state_cls = outputs[0][:, 0, :]
+
+        # Feed input to classifier to compute logits
+        logits = self.classifier(last_hidden_state_cls)
+
+        return logits
+
+from transformers import AdamW, get_linear_schedule_with_warmup
+
+def initialize_model(epochs=4):
+    """Initialize the Bert Classifier, the optimizer and the learning rate scheduler.
+    """
+    # Instantiate Bert Classifier
+    bert_classifier = BertClassifier(freeze_bert=False)
+
+    # Tell PyTorch to run the model on GPU
+    bert_classifier.to(device)
+
+    # Create the optimizer
+    optimizer = AdamW(bert_classifier.parameters(),
+                      lr=5e-5,    # Default learning rate
+                      eps=1e-8    # Default epsilon value
+                      )
+
+    # Total number of training steps
+    total_steps = len(train_dataloader) * epochs
+
+    # Set up the learning rate scheduler
+    scheduler = get_linear_schedule_with_warmup(optimizer,
+                                                num_warmup_steps=0, # Default value
+                                                num_training_steps=total_steps)
+    return bert_classifier, optimizer, scheduler
+
+import random
+import time
+
+# Specify loss function
+loss_fn = nn.CrossEntropyLoss()
+
+def set_seed(seed_value=42):
+    """Set seed for reproducibility.
+    """
+    random.seed(seed_value)
+    np.random.seed(seed_value)
+    torch.manual_seed(seed_value)
+    torch.cuda.manual_seed_all(seed_value)
+
+def train(model, train_dataloader, val_dataloader=None, epochs=4, evaluation=False):
+    """Train the BertClassifier model.
+    """
+    # Start training loop
+    print("Start training...\n")
+    for epoch_i in range(epochs):
+        # =======================================
+        #               Training
+        # =======================================
+        # Print the header of the result table
+        print(f"{'Epoch':^7} | {'Batch':^7} | {'Train Loss':^12} | {'Val Loss':^10} | {'Val Acc':^9} | {'Elapsed':^9}")
+        print("-"*70)
+
+        # Measure the elapsed time of each epoch
+        t0_epoch, t0_batch = time.time(), time.time()
+
+        # Reset tracking variables at the beginning of each epoch
+        total_loss, batch_loss, batch_counts = 0, 0, 0
+
+        # Put the model into the training mode
+        model.train()
+
+        # For each batch of training data...
+        for step, batch in enumerate(train_dataloader):
+            batch_counts +=1
+            # Load batch to GPU
+            b_input_ids, b_attn_mask, b_labels = tuple(t.to(device) for t in batch)
+
+            # Zero out any previously calculated gradients
+            model.zero_grad()
+
+            # Perform a forward pass. This will return logits.
+            logits = model(b_input_ids, b_attn_mask)
+
+            # Compute loss and accumulate the loss values
+            #print(type(b_labels))
+            loss = loss_fn(logits, b_labels)
+            batch_loss += loss.item()
+            total_loss += loss.item()
+
+            # Perform a backward pass to calculate gradients
+            loss.backward()
+
+            # Clip the norm of the gradients to 1.0 to prevent "exploding gradients"
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+
+            # Update parameters and the learning rate
+            optimizer.step()
+            scheduler.step()
+
+            # Print the loss values and time elapsed for every 20 batches
+            if (step % 20 == 0 and step != 0) or (step == len(train_dataloader) - 1):
+                # Calculate time elapsed for 20 batches
+                time_elapsed = time.time() - t0_batch
+
+                # Print training results
+                print(f"{epoch_i + 1:^7} | {step:^7} | {batch_loss / batch_counts:^12.6f} | {'-':^10} | {'-':^9} | {time_elapsed:^9.2f}")
+
+                # Reset batch tracking variables
+                batch_loss, batch_counts = 0, 0
+                t0_batch = time.time()
+
+        # Calculate the average loss over the entire training data
+        avg_train_loss = total_loss / len(train_dataloader)
+
+        print("-"*70)
+        # =======================================
+        #               Evaluation
+        # =======================================
+        if evaluation == True:
+            # After the completion of each training epoch, measure the model's performance
+            # on our validation set.
+            val_loss, val_accuracy = evaluate(model, val_dataloader)
+
+            # Print performance over the entire training data
+            time_elapsed = time.time() - t0_epoch
+            
+            print(f"{epoch_i + 1:^7} | {'-':^7} | {avg_train_loss:^12.6f} | {val_loss:^10.6f} | {val_accuracy:^9.2f} | {time_elapsed:^9.2f}")
+            print("-"*70)
+        print("\n")
+    
+    print("Training complete!")
+
+
+def evaluate(model, val_dataloader):
+    """After the completion of each training epoch, measure the model's performance
+    on our validation set.
+    """
+    # Put the model into the evaluation mode. The dropout layers are disabled during
+    # the test time.
+    model.eval()
+
+    # Tracking variables
+    val_accuracy = []
+    val_loss = []
+
+    # For each batch in our validation set...
+    for batch in val_dataloader:
+        # Load batch to GPU
+        b_input_ids, b_attn_mask, b_labels = tuple(t.to(device) for t in batch)
+
+        # Compute logits
+        with torch.no_grad():
+            logits = model(b_input_ids, b_attn_mask)
+
+        # Compute loss
+        loss = loss_fn(logits, b_labels)
+        val_loss.append(loss.item())
+
+        # Get the predictions
+        preds = torch.argmax(logits, dim=1).flatten()
+
+        # Calculate the accuracy rate
+        accuracy = (preds == b_labels).cpu().numpy().mean() * 100
+        val_accuracy.append(accuracy)
+
+    # Compute the average accuracy and loss over the validation set.
+    val_loss = np.mean(val_loss)
+    val_accuracy = np.mean(val_accuracy)
+
+    return val_loss, val_accuracy
+
+set_seed(42)    # Set seed for reproducibility
+bert_classifier, optimizer, scheduler = initialize_model(epochs=3)
+train(bert_classifier, train_dataloader, val_dataloader, epochs=3, evaluation=True)
+
+import torch.nn.functional as F
+
+def bert_predict(model, test_dataloader):
+    """Perform a forward pass on the trained BERT model to predict probabilities
+    on the test set.
+    """
+    # Put the model into the evaluation mode. The dropout layers are disabled during
+    # the test time.
+    model.eval()
+
+    all_logits = []
+
+    # For each batch in our test set...
+    for batch in test_dataloader:
+        # Load batch to GPU
+        b_input_ids, b_attn_mask = tuple(t.to(device) for t in batch)[:2]
+
+        # Compute logits
+        with torch.no_grad():
+            logits = model(b_input_ids, b_attn_mask)
+        all_logits.append(logits)
+    
+    # Concatenate logits from each batch
+    all_logits = torch.cat(all_logits, dim=0)
+
+    # Apply softmax to calculate probabilities
+    probs = F.softmax(all_logits, dim=1).cpu().numpy()
+
+    return probs
+
+
+# Compute predicted probabilities on the test set
+probs = bert_predict(bert_classifier, val_dataloader)
+
+# Evaluate the Bert classifier
+evaluate_roc(probs, y_val, 'bert.png')
---
+++ tf_jaen_ge.py
@@ -0,0 +1,295 @@
+"""
+Seq2Seq using Transformers on the Multi30k
+dataset. In this video I utilize Pytorch
+inbuilt Transformer modules, and have a
+separate implementation for Transformers
+from scratch. Training this model for a
+while (not too long) gives a BLEU score
+of ~35, and I think training for longer
+would give even better results.
+
+"""
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import spacy
+from utils import translate_sentence, bleu, save_checkpoint, load_checkpoint
+from torch.utils.tensorboard import SummaryWriter
+#from torchtext.datasets import Multi30k
+#from torchtext.data import Field, BucketIterator, TabularDataset
+from torchtext.data import Field, BucketIterator, TabularDataset
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+
+
+#spacy_ger = spacy.load("de_core_news_sm")
+spacy_eng = spacy.load("en_core_web_sm")
+spacy_jap = spacy.load("ja_core_news_sm")
+
+print("torkenizer is loaded")
+
+def tokenize_jap(text):
+    return [tok.text for tok in spacy_jap.tokenizer(text)]
+
+
+def tokenize_eng(text):
+    return [tok.text for tok in spacy_eng.tokenizer(text)]
+
+
+japanese = Field(tokenize=tokenize_jap, lower=True, init_token="<sos>", eos_token="<eos>")
+
+english = Field(
+    tokenize=tokenize_eng, lower=True, init_token="<sos>", eos_token="<eos>"
+)
+
+#there's no Japnaese dataset in Multi30k
+"""train_data, valid_data, test_data = Multi30k.splits(
+    exts=(".ja", ".en"), fields=(japanese, english)
+)"""
+
+import tarfile
+import os
+import re
+import pandas as pd
+path = './data/raw.tar.gz'
+savedir = './data/'
+with tarfile.open(path, 'r:*') as tar:
+    tar.extractall(savedir)
+
+from sklearn.model_selection import train_test_split
+
+filename = 'raw'
+with open(os.path.join(savedir + 'raw/', filename), 'r') as f:
+  raw_data = f.readlines()
+
+raw_list = [re.sub('\n', '', s).split('\t') for s in raw_data]
+raw_df = pd.DataFrame(raw_list,
+                  columns=['英語', '日本語'])
+
+train_val2, test_val = train_test_split(raw_df, test_size=0.3)
+train_val, validation_val = train_test_split(train_val2, test_size=0.3)
+
+train_val.to_csv('train.csv', index = False)
+validation_val.to_csv('validation.csv', index=False)
+test_val.to_csv('test.csv', index = False)
+
+print("start split")
+train_data, valid_data, test_data = TabularDataset.splits(
+    path='./',
+    train='train.csv',
+    validation='validation.csv',
+    test='test.csv',
+    format='csv',
+    fields=[('en_text', english), ('ja_text', japanese)]
+)
+#train_data, valid_data = train_test_split(train)
+print("end split")
+
+japanese.build_vocab(train_data, max_size=10000, min_freq=2)
+english.build_vocab(train_data, max_size=10000, min_freq=2)
+
+
+class Transformer(nn.Module):
+    def __init__(
+        self,
+        embedding_size,
+        src_vocab_size,
+        trg_vocab_size,
+        src_pad_idx,
+        num_heads,
+        num_encoder_layers,
+        num_decoder_layers,
+        forward_expansion,
+        dropout,
+        max_len,
+        device,
+    ):
+        super(Transformer, self).__init__()
+        self.src_word_embedding = nn.Embedding(src_vocab_size, embedding_size)
+        self.src_position_embedding = nn.Embedding(max_len, embedding_size)
+        self.trg_word_embedding = nn.Embedding(trg_vocab_size, embedding_size)
+        self.trg_position_embedding = nn.Embedding(max_len, embedding_size)
+
+        self.device = device
+        self.transformer = nn.Transformer(
+            embedding_size,
+            num_heads,
+            num_encoder_layers,
+            num_decoder_layers,
+            forward_expansion,
+            dropout,
+        )
+        self.fc_out = nn.Linear(embedding_size, trg_vocab_size)
+        self.dropout = nn.Dropout(dropout)
+        self.src_pad_idx = src_pad_idx
+
+    def make_src_mask(self, src):
+        src_mask = src.transpose(0, 1) == self.src_pad_idx
+
+        # (N, src_len)
+        return src_mask.to(self.device)
+
+    def forward(self, src, trg):
+        src_seq_length, N = src.shape
+        trg_seq_length, N = trg.shape
+
+        src_positions = (
+            torch.arange(0, src_seq_length)
+            .unsqueeze(1)
+            .expand(src_seq_length, N)
+            .to(self.device)
+        )
+
+        trg_positions = (
+            torch.arange(0, trg_seq_length)
+            .unsqueeze(1)
+            .expand(trg_seq_length, N)
+            .to(self.device)
+        )
+
+        embed_src = self.dropout(
+            (self.src_word_embedding(src) + self.src_position_embedding(src_positions))
+        )
+        embed_trg = self.dropout(
+            (self.trg_word_embedding(trg) + self.trg_position_embedding(trg_positions))
+        )
+
+        src_padding_mask = self.make_src_mask(src)
+        trg_mask = self.transformer.generate_square_subsequent_mask(trg_seq_length).to(
+            self.device
+        )
+
+        out = self.transformer(
+            embed_src,
+            embed_trg,
+            src_key_padding_mask=src_padding_mask,
+            tgt_mask=trg_mask,
+        )
+        out = self.fc_out(out)
+        return out
+
+
+# We're ready to define everything we need for training our Seq2Seq model
+load_model = False
+save_model = True
+
+# Training hyperparameters
+num_epochs = 100
+learning_rate = 3e-4
+batch_size = 32
+
+# Model hyperparameters
+src_vocab_size = len(japanese.vocab)
+trg_vocab_size = len(english.vocab)
+embedding_size = 512
+num_heads = 8
+num_encoder_layers = 3
+num_decoder_layers = 3
+dropout = 0.10
+max_len = 100
+forward_expansion = 4
+src_pad_idx = english.vocab.stoi["<pad>"]
+
+# Tensorboard to get nice loss plot
+writer = SummaryWriter("runs/loss_plot")
+step = 0
+
+train_iterator, valid_iterator, test_iterator = BucketIterator.splits(
+    (train_data, valid_data, test_data),
+    batch_size=batch_size,
+    sort_within_batch=True,
+    #sort_key=lambda x: len(x.src),
+    sort_key=lambda x: len(x.ja_text),
+    device=device,
+)
+
+model = Transformer(
+    embedding_size,
+    src_vocab_size,
+    trg_vocab_size,
+    src_pad_idx,
+    num_heads,
+    num_encoder_layers,
+    num_decoder_layers,
+    forward_expansion,
+    dropout,
+    max_len,
+    device,
+).to(device)
+
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+    optimizer, factor=0.1, patience=10, verbose=True
+)
+
+pad_idx = english.vocab.stoi["<pad>"]
+criterion = nn.CrossEntropyLoss(ignore_index=pad_idx)
+
+if load_model:
+    load_checkpoint(torch.load("my_checkpoint.pth.tar"), model, optimizer)
+
+sentence = "この道を進むと駅があります"
+
+for epoch in range(num_epochs):
+    print(f"[Epoch {epoch} / {num_epochs}]")
+
+    if save_model:
+        checkpoint = {
+            "state_dict": model.state_dict(),
+            "optimizer": optimizer.state_dict(),
+        }
+        save_checkpoint(checkpoint)
+
+    model.eval()
+    translated_sentence = translate_sentence(
+        model, sentence, japanese, english, device, max_length=50
+    )
+
+    print(f"Translated example sentence: \n {translated_sentence}")
+    model.train()
+    losses = []
+
+    for batch_idx, batch in enumerate(train_iterator):
+        # Get input and targets and get to cuda
+        inp_data = batch.ja_text.to(device)
+        target = batch.en_text.to(device)
+
+        # Forward prop
+        output = model(inp_data, target[:-1, :])
+
+        # Output is of shape (trg_len, batch_size, output_dim) but Cross Entropy Loss
+        # doesn't take input in that form. For example if we have MNIST we want to have
+        # output to be: (N, 10) and targets just (N). Here we can view it in a similar
+        # way that we have output_words * batch_size that we want to send in into
+        # our cost function, so we need to do some reshapin.
+        # Let's also remove the start token while we're at it
+        output = output.reshape(-1, output.shape[2])
+        target = target[1:].reshape(-1)
+
+        optimizer.zero_grad()
+
+        loss = criterion(output, target)
+        losses.append(loss.item())
+
+        # Back prop
+        loss.backward()
+        # Clip to avoid exploding gradient issues, makes sure grads are
+        # within a healthy range
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1)
+
+        # Gradient descent step
+        optimizer.step()
+
+        # plot to tensorboard
+        writer.add_scalar("Training loss", loss, global_step=step)
+        step += 1
+
+    mean_loss = sum(losses) / len(losses)
+    scheduler.step(mean_loss)
+
+# running on entire test data takes a while
+score = bleu(test_data[1:100], model, japanese, english, device)
+print(f"Bleu score {score * 100:.2f}")
